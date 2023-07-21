637ff9e903f1dd708bb642ab2c6ca1f314d0ca99
Osman Yiğit Sökel <108831247+yigitsokel1@users.noreply.github.com>
2023-07-21 18:23:47 +0300

    Update Dynamic Array in C.c

--- C Solutions/Arrays and Strings/Dynamic Array in C.c
+++ C Solutions/Arrays and Strings/Dynamic Array in C.c
@@ -14,9 +14,16 @@ int** total_number_of_pages;
 
 int main()
 {
+    int i;
     int total_number_of_shelves;
     scanf("%d", &total_number_of_shelves);
     
+    total_number_of_books = (int*)malloc(total_number_of_shelves*sizeof(int));
+    for(i=0;i<total_number_of_shelves;i++)
+        total_number_of_books[i] = 0;
+        
+    total_number_of_pages = (int**)malloc(total_number_of_shelves*sizeof(int*));
+        
     int total_number_of_queries;
     scanf("%d", &total_number_of_queries);
     
@@ -25,11 +32,20 @@ int main()
         scanf("%d", &type_of_query);
         
         if (type_of_query == 1) {
-            /*
-             * Process the query of first type here.
-             */
+            
             int x, y;
             scanf("%d %d", &x, &y);
+            
+            if(total_number_of_books[x] == 0){
+                total_number_of_pages[x] = (int*)malloc(sizeof(int));
+                total_number_of_pages[x][total_number_of_books[x]] = y;
+                total_number_of_books[x]++;
+            }else{
+                total_number_of_pages[x] = (int*)realloc(total_number_of_pages[x],(total_number_of_books[x]+1)*sizeof(int));
+                total_number_of_pages[x][total_number_of_books[x]] = y;
+                total_number_of_books[x]++;
+            }
+                
 
         } else if (type_of_query == 2) {
             int x, y;
